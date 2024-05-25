939838959e31d9354a5cf291af9e7ac00067b8d1
Sori <78161765+ldh6434@users.noreply.github.com>
2024-05-25 20:12:31 +0900

    Title: 남녀노소 구분짓기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240525/남녀노소 구분짓기/sex-and-age.py
+++ 240525/남녀노소 구분짓기/sex-and-age.py
@@ -1,11 +1,11 @@
 a = int(input())
 b = int(input())
-if a = 0 :
+if a == 0 :
     if a >= 19 :
         print(MAN)
     else :
         print(BOY)
-if a = 1:
+if a == 1:
     if a >= 19 :
         print(WOMAN)
     else : 
