948933f804624f8b1b9a65f1fb332581277e368d
Sori <78161765+ldh6434@users.noreply.github.com>
2024-05-25 20:09:40 +0900

    Title: 두 사람, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240525/두 사람/two-person.py
+++ 240525/두 사람/two-person.py
@@ -6,7 +6,7 @@ num2 = input().split()
 age2 = int(num2[0])
 sex2 = num2[1]
 
-if (age1 >= 19 and sex1 =='M') or (age2 >= 19 or sex2 == 'M') :
+if (age1 >= 19 and sex1 =='M') or  (age2 >= 19 and sex2 == 'M') :
     print(1)
 else :
     print(0)
