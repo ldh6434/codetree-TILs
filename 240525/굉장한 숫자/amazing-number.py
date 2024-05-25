9b7aa2e150bfa526ae09101502aa36b1be451282
Sori <78161765+ldh6434@users.noreply.github.com>
2024-05-25 19:50:16 +0900

    Title: 굉장한 숫자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240525/굉장한 숫자/amazing-number.py
+++ 240525/굉장한 숫자/amazing-number.py
@@ -1,5 +1,5 @@
 a = int(input())
-if a % 2 == 1 and a % 3 ==0 or (if a % 2==0 and a % 5 ==0):
+if (a % 2 == 1 and a % 3 ==0) or (if a % 2==0 and a % 5 ==0):
     print('true')
 else :
     print('false')
