cad1c4354739a6763583b84b61a965ed14ebd46d
Sori <78161765+ldh6434@users.noreply.github.com>
2024-05-25 20:57:10 +0900

    Title: 윤년인가, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240525/윤년인가/is-leap-year.py
+++ 240525/윤년인가/is-leap-year.py
@@ -1,9 +1,9 @@
 y = int(input())
 
 if y % 4 == 0:
-    if y % 100 == 0 and y % 400 == 0:
-        print('true')
-    else:
+    if y % 100 == 0 and y % 400 != 0:
         print('false')
+    else:
+        print('true')
 else:
     print('false')
