51a144b012c0d7aa8b8b45e35440c8cc459cc4b1
Sori <78161765+ldh6434@users.noreply.github.com>
2024-07-01 16:24:56 +0900

    Title: a 부터 b 까지, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240701/a 부터 b 까지/a-to-b.py
+++ 240701/a 부터 b 까지/a-to-b.py
@@ -1,9 +1,8 @@
 a, b= map(int, input().split())
-print(a, end=" ")
-while a <= b-1:
-    if a % 2 == 1:
-        a *= 2
-        print(a, end=' ')
-    elif a % 2 == 0:
-        a += 3
-        print(a, end=' ')
+num = a
+while num <= b:
+	print(num, end=" ")
+	if num % 2 == 1:
+		num *= 2
+	else:
+		num += 3
