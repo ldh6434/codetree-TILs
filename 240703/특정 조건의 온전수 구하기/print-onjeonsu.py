b805d03294ce1aa97166d45b1a311c4c0350cab3
Sori <78161765+ldh6434@users.noreply.github.com>
2024-07-03 16:19:25 +0900

    Title: 특정 조건의 온전수 구하기, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240703/특정 조건의 온전수 구하기/print-onjeonsu.py
+++ 240703/특정 조건의 온전수 구하기/print-onjeonsu.py
@@ -1,6 +1,6 @@
 n = int(input())
+
 for i in range(1,n+1):
-    if i % 2== 0 or i % 10 == 5 or i % 3 == 0 or i % 6 ==0 :
+    if i % 2 == 0 or i % 5 == 0 or (i % 3 == 0 and i % 9 != 0):
         continue
-    print(i,end=" ")
-    continue
+    print(i, end = ' ')
