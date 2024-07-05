db71c2a4c85c5f164f8f89f54b3a2e3c9d5d29f7
Sori <78161765+ldh6434@users.noreply.github.com>
2024-07-05 15:32:08 +0900

    Title: 소수 판별기, Time: 210ms, Memory: 24MB, Status: Passed - CodeTree

--- 240705/소수 판별기/prime-number-discriminator.py
+++ 240705/소수 판별기/prime-number-discriminator.py
@@ -1,9 +1,25 @@
-n = int(input())
-satisfied=True
-for i in range(2,n):
-    if i % n == 0 :
-        satisfied=False
-if satisfied==True:
-    print('P')
+import math
+
+# 소수 판별 함수
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+# 숫자 입력
+n = int(input().strip())
+
+# 소수 여부 출력
+if is_prime(n):
+    print("P")
 else:
-    print('C')
+    print("C")
