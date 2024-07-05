76341db1fa2644e48236dcd2f1fae43575fa013f
Sori <78161765+ldh6434@users.noreply.github.com>
2024-07-05 14:58:45 +0900

    Title: ab 사이에 없는 c, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240705/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 240705/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -1,9 +1,10 @@
 a, b, c = map(int,input().split())
 k = False
 for i in range (a,b+1):
-    if i % c == 0:
-        k = False
-if k == False:
+    if i % c != 0:
+        k = True
+        break
+if k == True:
     print('YES')
 else :
     print('NO')
